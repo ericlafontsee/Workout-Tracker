fc46331ab748770f4166f6d60c790167c6732431
Eric LaFontsee <elafontsee@gmail.com>
2020-10-07 17:27:45 -0500

    Added a workout.js file to the models folder and set up the schema

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+        day: {
+          type: Date,
+          default: Date.now
+        },
+        exercises: [
+          {
+            type: {
+              type: String,
+              trim: true,
+              required: "Enter a type of exercise!"
+            },
+            name: {
+              type: String,
+              trim: true,
+              required: "Enter a name for the exercise!"
+            },
+            duration: {
+              type: Number,
+              required: "Enter your time in number of minutes!"
+            },
+            sets: {
+              type: Number
+            },
+            reps: {
+              type: Number
+            },
+            weight: {
+              type: Number
+            },
+            distance: {
+              type: Number
+            }
+          }
+        ]
+      });
+  
+  const Workout = mongoose.model("Book", WorkoutSchema);
+  
+  module.exports = Book;
