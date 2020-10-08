55b22f7c937e44b56bdae9e7039c16849f629a33
Eric LaFontsee <elafontsee@gmail.com>
2020-10-08 14:47:38 -0500

    Added gzip

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const compression = require("compression");
 
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(compression());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
